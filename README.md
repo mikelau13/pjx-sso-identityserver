6e8ee410da0bf25c3bedb557af699d975c0424c4
mikelau13 <mikelau13@hotmail.com>
2020-07-08 11:58:25 -0400

    Initial commit - simple IdentityServer4 with MVC UI

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # pjx-sso-identityserver
-Single Sign On using IdentityServer4
+
+Single Sign On using [IdentityServer4](https://identityserver4.readthedocs.io/)
+
+### To Open the Project
+
+Use Visual Studio 2019, open Project with file `IdentityServer.csproj`
+
+### To Launch
+
+```bash
+dotnet run
+```
+
+Or Start Debugging inside Visual Studio.
+
+After server started run, you can navigate the discovery document - https://localhost:5001/.well-known/openid-configuration
+
+The default OpenID Connect protocol have been set up, you can navigate to the login page - https://localhost:5001/Account/Login
+
+TODO: Add OpenID Connect client and Oauth client to consume this SSO server.
---
+++ Startup.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IdentityServer
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // uncomment, if you want to add an MVC-based UI
+            services.AddControllersWithViews();
+
+            var builder = services.AddIdentityServer()
+                .AddInMemoryIdentityResources(Config.IdentityResources) // Register the identity resources of OpenID Connecct
+                .AddInMemoryApiScopes(Config.ApiScopes)
+                .AddTestUsers(IdentityServerHost.Quickstart.UI.TestUsers.Users) // sample UI also comes with an in-memory “user database”; navigate to the TestUsers class to see 2 users alice and bob
+                .AddInMemoryClients(Config.Clients);
+
+            builder.AddDeveloperSigningCredential();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+
+            // uncomment if you want to add MVC
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseIdentityServer();
+
+            // uncomment, if you want to add MVC-based
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
